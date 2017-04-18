8040d3fd32fdecd682461d6a7523cdc04cc4ee7f
Jack Moore <jmoore6786wjm@hotmail.com>
2017-04-18 14:23:51 -0400

    User Migration

--- db/migrate/20170418160500_create_users.rb
+++ db/migrate/20170418160500_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :firstname
+      t.string :lastname
+      t.string :location
       t.timestamps
     end
   end
